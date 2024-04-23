62f3fd37b1ec68bb5df5f23821e0ca18f738331a
BendikFortress <bendik@fortress.no>
2024-04-23 11:43:41 +0200

    center and fill bg video

--- components/BackgroundVideoEmbed.tsx
+++ components/BackgroundVideoEmbed.tsx
@@ -12,14 +12,16 @@ export default function BackgroundVideoEmbed({
   const src = `${embed}??h=1488b32ce1&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&autoplay=1&mute=1&lopp=1&background=1`;
 
   return (
-    <iframe
-      src={src}
-      width="1000%"
-      height="1000%"
-      frameBorder="0"
-      allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
-      title={title}
-      className="absolute -z-10 h-dvh"
-    ></iframe>
+    <div className="overflow-hidden h-screen w-screen absolute top-0 inset-x-0">
+      <iframe
+        src={src}
+        width="200%"
+        height="200%"
+        frameBorder="0"
+        allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
+        title={title}
+        className="absolute -z-10 h-dvh left-1/2 -translate-x-1/2 "
+      ></iframe>
+    </div>
   );
 }
